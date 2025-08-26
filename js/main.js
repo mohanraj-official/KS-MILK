acc296d6484256b81992e01bbba77530f34736e9
MOHANRAJ S <121940251+mohanraj-official@users.noreply.github.com>
2025-08-26 14:37:25 +0530

    all pages the menu bar settled for mobile width

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 function toggleMenu() {
   document.querySelector(".nav-links").classList.toggle("show");
 }
+
+//place order script
+const form = document.getElementById("orderForm");
+const popup = document.getElementById("popup");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // stop actual submit
+  popup.style.display = "flex"; // show popup
+});
+
+function closePopup() {
+  popup.style.display = "none";
+  form.reset(); // reset after closing
+}
+
+
+
+
+
+// confirm order script
+function showSuccess() {
+  document.getElementById("successPopup").style.display = "flex";
+}
+function closePopup() {
+  document.getElementById("successPopup").style.display = "none";
+  window.location.href = "index.html"; // redirect home
+}
