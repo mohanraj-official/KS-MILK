17b7eb3aa74053c2af8288efec74a232d79a511b
MOHANRAJ S <121940251+mohanraj-official@users.noreply.github.com>
2025-10-31 15:58:52 +0530

    service code changed

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,30 +3,22 @@
 importScripts("https://www.gstatic.com/firebasejs/10.13.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.1/firebase-messaging-compat.js");
 
-
-// ✅ Your Firebase config
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyDWlIcY8xsjAI72GWkiUEWzZpgQXY5CcfM",
   authDomain: "ks-milk-4551a.firebaseapp.com",
   projectId: "ks-milk-4551a",
   storageBucket: "ks-milk-4551a.firebasestorage.app",
   messagingSenderId: "463842826689",
   appId: "1:463842826689:web:2e04e8cbf137592e183740"
-};
+});
 
-// ✅ Initialize Firebase in Service Worker
-firebase.initializeApp(firebaseConfig);
-
-// ✅ Retrieve Firebase Messaging instance
 const messaging = firebase.messaging();
 
-// ✅ Background message handler
 messaging.onBackgroundMessage((payload) => {
   console.log('📩 Received background message:', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/assets/images/logo.png' // optional icon path
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  const { title, body, icon } = payload.notification;
+  self.registration.showNotification(title, {
+    body,
+    icon: icon || "https://mohanraj-official.github.io/KS-MILK/assets/images/logo.png"
+  });
 });
